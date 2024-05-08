a00707d8535ed412058b6a9dea299291dfeb6d29
michal-bisikiewicz <bisikiewiczmichal@gmail.com>
2024-05-08 20:49:39 +0200

    added two separated method for GET & POST

--- src/main/java/com/example/resttemplate/SampleShawnMendesServerProxy.java
+++ src/main/java/com/example/resttemplate/SampleShawnMendesServerProxy.java
@@ -26,7 +26,7 @@ public class SampleShawnMendesServerProxy {
     @Value("${sample-shawn-mendes-server.service.port}")
     int port;
 
-    public String makeRequest() {
+    public String makePostRequest() {
         UriComponentsBuilder builder = UriComponentsBuilder
                 .newInstance()
                 .scheme("http")
@@ -41,6 +41,33 @@ public class SampleShawnMendesServerProxy {
             ResponseEntity<String> response = restTemplate.exchange(
                     builder.build().toUri(),
                     HttpMethod.POST,
+                    httpEntity,
+                    String.class
+            );
+            return response.getBody();
+        } catch (RestClientResponseException exception) {
+            System.out.println(exception.getStatusText() + " " + exception.getStatusCode().value());
+        } catch (RestClientException exception) {
+            System.out.println(exception.getMessage());
+        }
+        return null;
+    }
+
+    public String makeGetRequest() {
+        UriComponentsBuilder builder = UriComponentsBuilder
+                .newInstance()
+                .scheme("http")
+                .host(url)
+                .port(port)
+                .path("/shawn/songs");
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add("requestId", "headerValue");
+        SampleShawnMendesRequest requestBody = new SampleShawnMendesRequest("hahahah123");
+        HttpEntity<SampleShawnMendesRequest> httpEntity = new HttpEntity<>(requestBody, httpHeaders);
+        try {
+            ResponseEntity<String> response = restTemplate.exchange(
+                    builder.build().toUri(),
+                    HttpMethod.GET,
                     httpEntity,
                     String.class
             );
